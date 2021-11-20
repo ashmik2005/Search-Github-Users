0ac842a368ff4d4af64feb640933fd463bb09012
ashmik <ashmik.harinkhede@gmail.com>
2021-11-20 11:12:29 +0530

    Calculate most used languages

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -3,26 +3,45 @@ import styled from "styled-components";
 import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  const { repos } = React.useContext(GithubContext); 
+  const { repos } = React.useContext(GithubContext);
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+    return total;
+  }, {});
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+  console.log(languages);
   const chartData = [
-  {
-    label: "HTML",
-    value: "13"
-  },
-  {
-    label: "CSS",
-    value: "160"
-  },
-  {
-    label: "Javascript",
-    value: "80"
-  },
-];
+    {
+      label: "HTML",
+      value: "13",
+    },
+    {
+      label: "CSS",
+      value: "160",
+    },
+    {
+      label: "Javascript",
+      value: "80",
+    },
+  ];
 
   return (
     <section className="section">
-      <Wrapper className="section-center"> 
-        <Pie3D data={chartData} />
+      <Wrapper className="section-center">
+        <Pie3D data={languages} />
         {/* <ExampleChart data={chartData} /> */}
       </Wrapper>
     </section>
