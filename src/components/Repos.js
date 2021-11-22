ac366d68ece9d96a9a838c7299e6d9f186371ff9
ashmik <ashmik.harinkhede@gmail.com>
2021-11-22 16:26:19 +0530

    Stars, Forks charts

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -36,7 +36,20 @@ const Repos = () => {
     })
     .slice(0, 5);
 
-  
+  // stars, forks 
+
+  let {stars, forks} = repos.reduce((total, item)=>{ 
+    const {stargazers_count,name,forks} = item; 
+    total.stars[stargazers_count] = {label:name,value:stargazers_count} 
+    total.forks[forks] = {label:name,value:forks}
+    return total;
+  },{ 
+    stars:{}, 
+    forks:{}
+  }) 
+
+  stars = Object.values(stars).slice(-5).reverse(); 
+  forks = Object.values(forks).slice(-5).reverse();
 
   const chartData = [
     {
@@ -57,9 +70,9 @@ const Repos = () => {
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
-        <div></div>
-        <Doughnut2D data={mostPopular} />
-        <div></div>
+        <Column3D data={stars}/>
+        <Doughnut2D data={mostPopular} /> 
+        <Bar3D data={forks}/>
       </Wrapper>
     </section>
   );
